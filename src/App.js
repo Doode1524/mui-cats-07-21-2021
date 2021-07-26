adcb4e30f54b59ff65d64ba5fc4d8d9c94ea8103
joey <doode1524@gmail.com>
2021-07-25 20:49:11 -0400

    overrides paragraph

--- src/App.js
+++ src/App.js
@@ -1,45 +1,28 @@
-import logo from "./logo.svg";
-import "./App.css";
-import Header from "./Header";
 import { makeStyles } from "@material-ui/core/styles";
-import { useTheme } from "@material-ui/core/styles";
 import { createTheme } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import { ThemeProvider } from "@material-ui/core/styles";
-import Cats from "./Cats";
 
 function App() {
   const theme = createTheme({
-    typography: {
-      h1: {
-        color: "blue",
-        fontSize: "4rem",
+    overrides: {
+      MuiTypography: {
+        paragraph: {
+          color: "red",
+        },
       },
     },
   });
 
-  const useStyles = makeStyles({
-    h1: {
-      color: "red",
-      marginLeft: "10px",
-    },
-    h2: {
-      color: "blue",
-      border: "2px solid red",
-    },
-  });
-  const classes = useStyles();
-
   return (
     <div>
-      <Header />
       <ThemeProvider theme={theme}>
-        <Cats />
-        <h1 className={classes.h1}>Hello header</h1>
-        {/* <Typography variant='h1'>Hello Typography</Typography> */}
+        <Typography paragraph>This is in place of a p tag</Typography>
       </ThemeProvider>
     </div>
   );
 }
 
 export default App;
+
+
